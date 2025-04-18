9bff52291aa6bb4ca6c2a0cee75c8736d96e6f34
Halit <halitum0216@gmail.com>
2025-04-18 12:03:25 +0800

    添加模型

--- configs/model_config.py
+++ configs/model_config.py
@@ -2,9 +2,9 @@ mem0_ai_api = ""
 
 llm_model = {
     "deepseek-v3": {
-        "model_name": "",
-        "api_base_url": "",
-        "api_key": "",
+        "model_name": "DeepSeek-V3",
+        "api_base_url": "https://one-api.maas.com.cn/v1/",
+        "api_key": "sk-8IGJQBsUBzTbXmNACeAdB3B2Af8344A9B4179040B5814cDf",
         "openai_proxy": "",
         "type": "instruction",
     }
